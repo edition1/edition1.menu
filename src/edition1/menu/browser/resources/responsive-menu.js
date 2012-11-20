400432efda529db74b4d0dabe2fb14c966c5b388
Mark van Lent <mark@vlent.nl>
2012-11-20 14:17:45 +0100

    Factor out setting the menu class (small-menu, big-menu).
    This way we gan also call the function when the size of the window
    changes.

--- src/edition1/menu/browser/resources/responsive-menu.js
+++ src/edition1/menu/browser/resources/responsive-menu.js
@@ -1,12 +1,23 @@
-jq(document).ready(function() {
-    var min_width = jq('#responsive-menu').attr('data-min-width');
+function setMenuSize() {
+    var menu = jq('#responsive-menu');
+    var min_width = menu.attr('data-min-width');
     if (typeof min_width == 'undefined') {
         min_width = 0;
     }
-
-    var menu_type = 'small-menu';
+    var newMenuType = 'small-menu';
+    var oldMenuType = 'big-menu';
     if (jq(window).width() >= min_width) {
-        menu_type = 'big-menu';
+        newMenuType = 'big-menu';
+        oldMenuType = 'small-menu';
     }
-    jq('#responsive-menu').addClass(menu_type);
+    menu.addClass(newMenuType).removeClass(oldMenuType);
+}
+
+jq(window).resize(function() {
+    setMenuSize();
+});
+
+
+jq(document).ready(function() {
+    setMenuSize();
 });
