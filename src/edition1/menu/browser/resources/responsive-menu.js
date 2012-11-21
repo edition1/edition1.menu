46bb1d0bd5d83b4abdbbc95d52d12c07319d1c40
Mark van Lent <mark@vlent.nl>
2012-11-21 14:52:40 +0100

    Implement the folding/unfolding of the submenus.

--- src/edition1/menu/browser/resources/responsive-menu.js
+++ src/edition1/menu/browser/resources/responsive-menu.js
@@ -1,4 +1,21 @@
+function resetSmallMenu() {
+    // Reset the small menu so all items are 'collapsed'.
+    jq('.small-menu .has-children').removeClass('children-visible')
+                                   .addClass('children-hidden');
+    jq('.small-menu .has-children').toggle(function(event) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        jq(this).removeClass('children-hidden').addClass('children-visible');
+        jq(this).unbind();
+    }, function () {
+        // Don't do anything, let the browser handle the click on the link.
+        return true;
+    });
+}
+
 function setMenuSize() {
+    // Set a class on the menu which determines if the small or big menu
+    // should be shown.
     var menu = jq('#responsive-menu');
     var min_width = menu.attr('data-min-width');
     if (typeof min_width == 'undefined') {
@@ -11,12 +28,12 @@ function setMenuSize() {
         oldMenuType = 'small-menu';
     }
     menu.addClass(newMenuType).removeClass(oldMenuType);
+    resetSmallMenu();
 }
 
 jq(window).resize(function() {
     setMenuSize();
 });
-
 
 jq(document).ready(function() {
     setMenuSize();
